c53b512ce57b7de70340eb3746c2e608717a3e12
victor barbier <victor.barbier@recherche.gouv.fr>
2024-08-02 09:58:49 +0200

    feat(chat): custom chat bubbles

--- client/src/pages/chat/components/messages.tsx
+++ client/src/pages/chat/components/messages.tsx
@@ -1,29 +1,36 @@
-import { Container, Badge, Row } from "@dataesr/dsfr-plus"
-import { ChatMessages, ChatRole } from "../../../types/chat"
+import { Container, Row, Text } from "@dataesr/dsfr-plus"
+import { ChatMessage, ChatMessages } from "../../../types/chat"
+
+function Message({ message }: { message: ChatMessage }) {
+  const horizontalAlign = message.role === "user" ? "right" : "left"
+
+  return (
+    <Row horizontalAlign={horizontalAlign}>
+      <Text
+        style={{
+          fontSize: "15px",
+          whiteSpace: "pre-line",
+          borderRadius: "0.7em",
+          padding: "0.5em",
+          backgroundColor: message.role === "user" ? "#417DC4" : "#C8AA39",
+          color: "#fff",
+        }}
+      >
+        {message.content}
+      </Text>
+    </Row>
+  )
+}
 
 export default function Messages({ messages, isFetching }: { messages: ChatMessages; isFetching: boolean }) {
-  const roleColor = (role: ChatRole) => (role === "user" ? "blue-cumulus" : "yellow-tournesol")
-
   function Fetching() {
-    return isFetching ? (
-      <Row>
-        <Badge className="fr-mb-1w" color={roleColor("assistant")}>
-          ...
-        </Badge>
-      </Row>
-    ) : null
+    return isFetching ? <Message message={{ role: "assistant", content: ". . ." }} /> : null
   }
 
   return (
     <Container>
       {messages.map((message, index) => (
-        <Row key={index}>
-          <Badge
-            className="fr-mb-1w"
-            color={roleColor(message.role)}
-            key={index}
-          >{`(${message.role}) ${message.content}`}</Badge>
-        </Row>
+        <Message key={index} message={message} />
       ))}
       <Fetching />
     </Container>
