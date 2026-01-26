c2081772f12252ccac2982578ac9a292fa54e4f8
ahonestla <vicozer@gmail.com>
2026-01-26 12:04:15 +0100

    feat(networks): center error204

--- client/src/pages/networks/components/errors/error204.tsx
+++ client/src/pages/networks/components/errors/error204.tsx
@@ -5,15 +5,18 @@ export default function Error204() {
   const intl = useIntl()
 
   return (
-    <Container>
+    <Container
+      fluid
+      style={{ height: "100%", width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
       <Row>
-        <Col offsetMd="2">
+        <Col md="8">
           <Title as="h1" look="h5">
             {intl.formatMessage({ id: "networks.search.error204.title" })}
           </Title>
           <Text>{intl.formatMessage({ id: "networks.search.error204.text" })}</Text>
         </Col>
-        <Col>
+        <Col md="4">
           <svg className="fr-artwork" aria-hidden="true" viewBox="0 0 80 80" width="100px" height="auto">
             <use className="fr-artwork-decorative" href="/artwork/pictograms/system/warning.svg#artwork-decorative" />
             <use className="fr-artwork-minor" href="/artwork/pictograms/system/warning.svg#artwork-minor" />
