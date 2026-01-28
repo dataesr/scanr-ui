0d8d18dd4e2295a6e055bdc395f690fd3bc13a81
ahonestla <vicozer@gmail.com>
2026-01-28 14:54:50 +0100

    feat(organizations): do not show publications count if 0 in filter

--- client/src/pages/search/components/publications/filters/organizations.tsx
+++ client/src/pages/search/components/publications/filters/organizations.tsx
@@ -86,7 +86,11 @@ export default function PublicationOrganizationsFilter() {
         {(item) => (
           <AutocompleteItem
             startContent={<span className="fr-mr-3v fr-icon--md fr-icon-user-line" />}
-            endContent={<span className="fr-text--xs fr-text-mention--grey">{item.publicationsCount} publications</span>}
+            endContent={
+              item.publicationsCount ? (
+                <span className="fr-text--xs fr-text-mention--grey">{item.publicationsCount} publications</span>
+              ) : null
+            }
             description={item.address?.find((a) => a.main)?.city}
             key={`${getLangFieldValue(locale)(item.label)}###${[
               ...new Set([item.id, ...(item.externalIds?.map((id) => id.id) || [])]), // force first id to be the main one
