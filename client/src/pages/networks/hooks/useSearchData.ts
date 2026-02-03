bc7de0e5617e631f31f476ab011d1af148a76f34
ahonestla <vicozer@gmail.com>
2026-02-03 10:40:46 +0100

    feat(networks): return previous data when only clusters param changed

--- client/src/pages/networks/hooks/useSearchData.ts
+++ client/src/pages/networks/hooks/useSearchData.ts
@@ -5,6 +5,7 @@ import { networkSearch } from "../../../api/networks/search/search"
 import useUrl from "../../search/hooks/useUrl"
 import useIntegration from "./useIntegration"
 import useOptions from "./useOptions"
+import { NetworkParameters } from "../../../types/network"
 
 export default function useSearchData(forceClusters?: boolean) {
   const { currentQuery, filters } = useUrl()
@@ -15,8 +16,9 @@ export default function useSearchData(forceClusters?: boolean) {
 
   if (forceClusters !== undefined) parameters.clusters = forceClusters
 
+  const currentKey = ["network", "search", currentSource, currentModel, currentQuery, lang, filters, parameters]
   const { data, error, isFetching } = useQuery({
-    queryKey: ["network", "search", currentSource, currentModel, currentQuery, filters, lang, parameters],
+    queryKey: currentKey,
     queryFn: () =>
       networkSearch({
         source: currentSource,
@@ -27,7 +29,17 @@ export default function useSearchData(forceClusters?: boolean) {
         integration: integrationId,
         filters,
       }),
-    placeholderData: (previousData) => previousData,
+    placeholderData: (previousData, previousQuery) => {
+      // return previous data if only clusters parameter changed
+      const previousKey = previousQuery?.queryKey
+      if (previousKey && previousKey.slice(0, 6).every((value, index) => value === currentKey[index])) {
+        const previousParameters = previousQuery?.queryKey[7] as NetworkParameters
+        if (previousParameters?.clusters !== parameters.clusters) {
+          return previousData
+        }
+      }
+      return undefined
+    },
   })
 
   const values = useMemo(() => {
