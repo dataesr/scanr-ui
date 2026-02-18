4101028c2e7d6a65c0f3fc5615781de7b1bdf5a0
ahonestla <vicozer@gmail.com>
2026-02-18 10:31:53 +0100

    test(networks): export quadrants vector data

--- client/src/pages/networks/components/select-term/index.tsx
+++ client/src/pages/networks/components/select-term/index.tsx
@@ -1,11 +1,13 @@
-import { Container, SegmentedControl, SegmentedElement } from "@dataesr/dsfr-plus"
+import { Button, Container, SegmentedControl, SegmentedElement } from "@dataesr/dsfr-plus"
 import { useNetworkContext } from "../../context"
+import { useExportQuadrants } from "../../hooks/useExportQuadrants"
 
 export default function NetworkSelectTerm() {
   const {
     search: { n },
     options: { selectedTerm, setSelectedTerm },
   } = useNetworkContext()
+  const { isExporting, exportFile } = useExportQuadrants()
 
   if (n < 2) return null
 
@@ -23,6 +25,15 @@ export default function NetworkSelectTerm() {
         <SegmentedElement label={`2017 - 2023`} value={String(1)} checked={selectedTerm === 1} />
         <SegmentedElement label={`2020 - 2026`} value={String(2)} checked={selectedTerm === 2} />
       </SegmentedControl>
+      <Button
+        className="fr-ml-2w"
+        variant="text"
+        name="export"
+        onClick={async () => await exportFile("xlsx")}
+        disabled={isExporting}
+        icon="download-line"
+        iconPosition="right"
+      />
     </Container>
   )
 }
