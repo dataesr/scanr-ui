2d9a679a5d340210000e96535b39e9c65deae3cc
Anne L'Hôte <anne.lhote@gmail.com>
2026-06-16 17:27:34 +0200

    feat(clinical-trials): Enable it in prod

--- client/src/layout/components/header/index.tsx
+++ client/src/layout/components/header/index.tsx
@@ -121,14 +121,12 @@ export default function Header() {
 					>
 						{intl.formatMessage({ id: "layout.header.nav.search.patents" })}
 					</Link>
-					{!isInProduction() && (
-						<Link
-							current={pathname.split("/").includes("clinical-trials")}
-							href="/search/clinical-trials"
-						>
-							{intl.formatMessage({ id: "layout.header.nav.search.clinical-trials" })}
-						</Link>
-					)}
+					<Link
+						current={pathname.split("/").includes("clinical-trials")}
+						href="/search/clinical-trials"
+					>
+						{intl.formatMessage({ id: "layout.header.nav.search.clinical-trials" })}
+					</Link>
 				</NavItem>
 				<NavItem
 					current={
