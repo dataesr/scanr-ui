2d718d588554153957d841905afdef239106da0f
Anne L'Hôte <anne.lhote@gmail.com>
2026-06-17 11:01:42 +0200

    fix(identifiers): If no type display only id value

--- client/src/components/identifiers/index.tsx
+++ client/src/components/identifiers/index.tsx
@@ -3,12 +3,13 @@ import CopyBadge from "../copy/copy-badge";
 
 export default function Identifiers({ data: externalIds }: { data: ExternalIdsData[] }) {
   if (!externalIds?.length) return null;
+
   return (
     <div>
       <div className="fr-badge-group">
         {externalIds
           ?.filter((ext) => ext?.type !== 'scanr')
-          .map((ext) => <CopyBadge key={ext.id} lowercase size="sm" copyText={ext.id?.replace("idref", "")}>{`${ext.type.toUpperCase()} : ${ext.id?.replace("idref", "")}`}</CopyBadge>)
+          .map((ext) => <CopyBadge key={ext.id} lowercase size="sm" copyText={ext.id?.replace("idref", "")}>{ext?.type ? `${ext?.type?.toUpperCase()} : ` : ''}{`${ext.id?.replace("idref", "")}`}</CopyBadge>)
         }
       </div>
     </div>
