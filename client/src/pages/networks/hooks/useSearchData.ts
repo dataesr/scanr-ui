a081558d34fdc9c1e3f612ccd989c6ff2fe9e71e
victor barbier <vicozer@gmail.com>
2026-06-22 10:42:41 +0200

    fix(networks): useSearchData return previous data only if clusters changed

--- client/src/pages/networks/hooks/useSearchData.ts
+++ client/src/pages/networks/hooks/useSearchData.ts
@@ -5,6 +5,7 @@ import useUrl from "../../search/hooks/useUrl"
 import useIntegration from "./useIntegration"
 import useOptions from "./useOptions"
 import { getMultipleNetworks } from "../../../api/networks"
+import { NetworkParameters } from "../../../types/network"
 
 export default function useSearchData(forceClusters?: boolean) {
   const { currentQuery, filters } = useUrl()
@@ -31,10 +32,12 @@ export default function useSearchData(forceClusters?: boolean) {
         nfilters,
       }),
     placeholderData: (previousData, previousQuery) => {
-      // return previous data if only nfilters and parameters changed
+      // return previous data if only clusters changed
+      const currentParameters = currentKey[8] as NetworkParameters
       if (
         previousQuery?.queryKey &&
-        previousQuery.queryKey.slice(0, 6).every((value, index) => value === currentKey[index])
+        previousQuery.queryKey.slice(0, 7).every((value, index) => value === currentKey[index])
+        && previousQuery[8].clusters !== currentParameters.clusters
       ) {
         return previousData
       }
